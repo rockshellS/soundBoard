9ca9f302d4d54f0646611e5c2142835c95b7697d
rockshellS <spicer.rachelle@gmail.com>
2021-09-28 12:38:24 -0400

    drums

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['Hihatb1', 'Hihatopen', 'Hihatopen3', 'BassDrum3b', 'FloortomA', 'MedtomB','SmalltomB','SnareB', 'CymbalcrashA']
 
-sounds.forEach(sound => {
+for (let i = 0; i < sounds.length; i++) {
+    console.log(i)
     const btn = document.createElement('button')
     btn.classList.add('btn')
 
-    btn.innerText = sound
-
-    btn.addEventListener('click', () => {
-        document.getElementById(sound).play()
+    btn.innerText = sounds[i]
+    
+    btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        document.getElementById(sounds[i]).play()
+        btn.removeEventListener("click")
     })
-
     document.querySelector("#buttons").appendChild(btn)
-})
+}
